1cf45ad21283d637191cb14a92b2623273c3dff3
Vadim Gusak <gusakvd@gmail.com>
2023-01-04 19:10:59 +1000

    Update Book logic

--- tbot_random_book/book.py
+++ tbot_random_book/book.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 
 GOOD_STATUS_CODE_LIMIT = 299
+ERROR = 'Упс, что-то пошло не так. Попробуйте ещё раз.'
 
 
 def load_book(url: str) -> dict | None:
@@ -16,14 +17,15 @@ def load_book(url: str) -> dict | None:
         return None
 
     soup = BeautifulSoup(resp.content.decode('utf-8', 'ignore'), 'lxml')
-    result['title'] = soup.find('h1').text.strip()
-    result['author'] = soup.find('h2').text.strip()
+    result['title'] = soup.find('h1').text.strip() if soup.find('h1') else ''
+    result['author'] = soup.find('h2').text.strip() if soup.find('h2') else ''
     result['description'] = str(
         soup.find(id='lenta-card__text-edition-escaped').text.strip()
-    )
-    result['img'] = soup.find(id="main-image-book")['src']
+    ) if soup.find(id='lenta-card__text-edition-escaped') else ''
+    result['img'] = soup.find(id="main-image-book")['src'] \
+        if soup.find(id="main-image-book") else ''
 
-    return result if result else None
+    return result
 
 
 class Book(object):
@@ -34,15 +36,17 @@ class Book(object):
     @property
     def description(self) -> str:
         if self.book is None:
-            return 'Упс, что-то пошло не так. Попробуйте ещё раз.'
-        else:
-            return '\n'.join(
-                [
-                    self.book.get('title', ''),
-                    self.book.get('author', ''),
-                    self.book.get('description', '')
-                ]
-            )
+            return ERROR
+
+        result = '\n'.join(
+            [
+                self.book.get('title', ''),
+                self.book.get('author', ''),
+                self.book.get('description', '')
+
+            ]).strip()
+
+        return result if result else ERROR
 
     @property
     def image_link(self) -> str | None:
