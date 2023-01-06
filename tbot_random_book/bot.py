4ae979c331f593d6427dd3a282656492bd08422f
Vadim Gusak <gusakvd@gmail.com>
2023-01-06 10:13:47 +1000

    add logging

--- tbot_random_book/bot.py
+++ tbot_random_book/bot.py
@@ -1,6 +1,7 @@
 from telebot import TeleBot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from tbot_random_book.book import Book
+import logging
 
 
 START_MESSAGE = 'Нажмите кнопку или отправьте любой текст в чат, ' \
@@ -9,6 +10,12 @@ BUTTON = 'Случайная книга'
 
 
 def start_bot(token: str, url: str):
+    logging.basicConfig(
+        level=logging.INFO,
+        filename="bot_log.log",
+        format="%(asctime)s %(levelname)s %(message)s"
+    )
+
     bot = TeleBot(token)
     markup = ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     button = KeyboardButton(BUTTON)
@@ -23,6 +30,10 @@ def start_bot(token: str, url: str):
     @bot.message_handler(func=lambda message: True)
     def echo_all(message):
         book = Book(url)
+
+        log_mess = f'chat_id = {message.chat.id}, book = \n{book.description}\n'
+        logging.info(log_mess)
+
         bot.send_photo(
             chat_id=message.chat.id,
             photo=book.image_link,
